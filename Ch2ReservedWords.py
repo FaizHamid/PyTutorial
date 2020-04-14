addb45409b2a1ec9b1b6df5a2ef559fe16445d0f
FaizHamid <hamidmfaiz@gmail.com>
2020-04-14 08:01:13 -0400

    Syntax Patterns in Python

--- Ch2ReservedWords.py
+++ Ch2ReservedWords.py
@@ -39,6 +39,3 @@
 # with
 # yield
 
-x = 2  # Assignment Statement, "x" is a Variable, "2" is a Constant
-x = x + 2  # Assignment with expression, "=" and "+" are Operators
-print(x)  # Print Statement, "print" is a Reserved Word and also a Function
---
+++ Ch3Syntax.py
@@ -0,0 +1,3 @@
+x = 2  # Assignment Statement, "x" is a Variable, "2" is a Constant
+x = x + 2  # Assignment with expression, "=" and "+" are Operators
+print(x)  # Print Statement, "print" is a Reserved Word and also a Function
