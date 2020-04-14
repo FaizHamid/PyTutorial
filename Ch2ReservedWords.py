f3179dd67ed9969e20baaa9f4d2d16926ef20a86
FaizHamid <hamidmfaiz@gmail.com>
2020-04-14 07:59:18 -0400

    Reserved Words in Python

---
+++ Ch2ReservedWords.py
@@ -0,0 +1,44 @@
+# Chapter 2
+# Programming Language is like learning all other natural language
+# where Vocabulary = "Variable" and Words = "Reserved Words"
+
+# You cannot use reserved words as "Variable Name"  / "Identifiers"
+
+# These are the reserved words
+# False
+# None
+# True
+# and
+# as
+# assert
+# break
+# class
+# if
+# def
+# del
+# elif
+# else
+# except
+# return
+# for
+# from
+# global
+# try
+# import
+# in
+# is
+# lambda
+# while
+# not
+# or
+# pass
+# raise
+# finally
+# continue
+# nonlocal
+# with
+# yield
+
+x = 2  # Assignment Statement, "x" is a Variable, "2" is a Constant
+x = x + 2  # Assignment with expression, "=" and "+" are Operators
+print(x)  # Print Statement, "print" is a Reserved Word and also a Function
