9ea896e841afb6be7b8b1cd6f1cc3161d06aeec2
FaizHamid <hamidmfaiz@gmail.com>
2020-04-14 08:11:50 -0400

    Python Scripts and best practices

---
+++ Ch4Scripts.py
@@ -0,0 +1,5 @@
+# Chapter 4
+# Interactive Python is good for experiments and programs of 3 to 4 lines long
+# Most programs are much longer, so we type them into a file and tell Python to run the commands in the file
+# In a sentence we are "giving Python a Script"
+# As a conversation, we add ".py" as the suffix on the end of these files to indicate they contain Python programs.
