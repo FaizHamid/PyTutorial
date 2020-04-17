85f7fdbb9216b809d6ceda1661d4311f3b31aaa7
FaizHamid <hamidmfaiz@gmail.com>
2020-04-17 16:55:02 -0400

    If - conditional statement

---
+++ IfStatement.py
@@ -0,0 +1,10 @@
+# If (Conditional Steps) is conditional statement. If the condition is true, then do the indented statements.
+# If the condition is not true, then skip the indented statements.
+
+x = 5
+if x < 10:
+    print('Smaller')
+if x > 20:
+    print('Bigger')
+
+print('Finish')
