ea405b24223ececb013a177fa21724e92785aa1b
FaizHamid <hamidmfaiz@gmail.com>
2020-04-25 05:51:02 -0400

    opening a file..

---
+++ PythonDoc/Ch7ReadFiles.py
@@ -0,0 +1,8 @@
+# Chapter 7 - Reading Files
+
+# OPENING A FILE
+# before we can read the contents of the file, we must tell Python which file we are going to work with and what we will be doing with the file
+# this is done with the 'open()' function
+# open() returns a "file handle" - a variable used to perform operations on the file
+# Similar to "File -> Open" in a Word Processor
+
